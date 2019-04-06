2198e1fc2dfb213db9e209cf6ce6c6a6412311bd
Justin <justinknueppel@gmail.com>
2019-04-06 11:02:46 -0400

    Added title to make posts make sense

--- ShareReddit.py
+++ ShareReddit.py
@@ -7,6 +7,7 @@ subreddit = 'aww'
 req = requests.get(f'https://www.reddit.com/r/{subreddit}/top.json?t=day', headers = {'User-agent' : 'Justin'})
 js = req.json()
 url = js['data']['children'][0]['data']['url']
+title = js['data']['children'][0]['data']['title']
 # print(url)
 
 
@@ -14,4 +15,4 @@ client = Client(info.accountSID, info.authToken)
 
 for person in info.people:
     message = choice(person['Messages'])
-    response = client.messages.create(body=f'\n\n{message} \n{url}', from_=info.myTwilioNumber, to=person['Phone Number'])
+    response = client.messages.create(body=f'\n\n{message} \n{title}\n{url}', from_=info.myTwilioNumber, to=person['Phone Number'])
