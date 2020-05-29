bf4d68ed6ee0a83dbe2732acb05e8ef3f6d2e115
Justin <justinknueppel@gmail.com>
2020-05-29 12:12:50 -0400

    update automate script

--- automate.sh
+++ automate.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
 DATE_STRING=$(date +'%Y-%m-%d')
 mkdir -p logs
 LOGFILE="logs/$DATE_STRING.log"
 touch $LOGFILE
-docker build . -t post-share > $LOGFILE 2>&1
-docker run post-share > $LOGFILE 2>&1
+docker build . -t post-share && docker run post-share > $LOGFILE 2>&1
